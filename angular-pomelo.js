10ccaa2e7a5922912845ed668b3931bf280ae00e
Nemo <viole_day@live.com>
2015-05-18 14:28:31 +0800

    fix something

--- angular-pomelo.js
+++ angular-pomelo.js
@@ -3,38 +3,36 @@
  */
 angular.module('angular-pomelo', [])
   .run([function () {
-    if (typeof pomelo === 'undefined') {
+    if (angular.isUndefined(pomelo)) {
       throw new Error('pomelo is not defined')
     }
   }])
-  .factory('ngPomelo', ['$q', function ($q) {
-    var ngPomelo = {};
-    ngPomelo.init = function (config) {
+  .factory('pomelo', ['$q', function ($q) {
+
+    pomelo.initialize = function (config) {
       if (!angular.isArray(config.gate) || config.gate.length == 0) {
         throw new Error('gate list must be an array');
       }
       this.config = config;
     };
 
-    ngPomelo.getGate = function () {
+    pomelo.getGate = function () {
       var gate = this.config.gate;
       var index = Math.floor(Math.random() * (gate.length));
       return gate[index];
     };
 
-    ngPomelo.queryEntry = function (uid) {
+    pomelo.queryEntry = function (body) {
       var deferred = $q.defer();
       var gate = this.getGate();
       pomelo.init(gate, function () {
-        pomelo.request('gate.gateHandler.queryEntry', {
-          uid: uid
-        }, function (data) {
+        pomelo.request('gate.gateHandler.queryEntry', body, function (data) {
           pomelo.disconnect();
           if (data.code === 500) {
             deferred.reject(new Error(data));
           } else {
             pomelo.init(data, function () {
-              pomelo.request('connector.entryHandler.enter', {uid: uid}, function (data) {
+              pomelo.request('connector.entryHandler.enter', body, function (data) {
                 if (data.error) {
                   deferred.reject(new Error(data.error));
                 } else {
@@ -48,5 +46,5 @@ angular.module('angular-pomelo', [])
       return deferred.promise;
     };
 
-    return ngPomelo;
+    return pomelo;
   }]);
